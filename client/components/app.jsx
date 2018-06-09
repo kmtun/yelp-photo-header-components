5d3d700160d7b3b0c6d6c8029d951b2f8547652e
Kevin <kevintun1122@gmail.com>
2018-06-08 17:49:16 -0700

    - Finished yelp header - Restaurant details in progress - Currently working on name, review stars, categories

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -10,59 +10,124 @@ class App extends Component {
     this.state = {
       static_nav_links: ['Restaurants', 'Nightlife', 'Home Services'],
       static_nav_links_2: ['Write a review', 'Events', 'Talk', 'Collections'],
+      categories: ['Sandwiches', 'Salad', 'American (New)'],
     }
   }
   render() {
     const nav_bar_icons = ['fas fa-utensils', 'fas fa-glass-martini', 'fas fa-screwdriver']
-    return (
-      <div style = {{backgroundSize: '1135px auto, 100%'}}>
-        <css.header.h>
-          <css.header.h_content>
-            
-            <css.header.logo>
-            </css.header.logo>
 
-            <css.header.search type="text" placeholder="tacos, cheap dinner, Max's">
-            </css.header.search>
+    return (
+      <div>
+        <css.header.h>
+
+          <css.header.h_content>
+
+            <css.header_spacing>
+              <css.header.logo>
+              </css.header.logo>
+            </css.header_spacing>
+
+            <css.header_spacing>
+              <css.header.search type="text" placeholder="tacos, cheap dinner, Max's">
+              </css.header.search>
+            </css.header_spacing>
 
             <css.header.search type="text" placeholder="Los Angeles, CA">
             </css.header.search>
 
-            <css.header.search_button primary>
+            <css.header.search_button>
               <span> <i className="fas fa-search"></i> </span>
             </css.header.search_button>
-        
+
+            <css.header.divForSignup>
+              <css.header.signup_button href="/">
+                Sign up
+             </css.header.signup_button>
+            </css.header.divForSignup>
 
           </css.header.h_content>
+
         </css.header.h>
 
         <css.header.nav>
-          <css.header_padding>
-            <css.header.nav_content_header>
-              <css.header.nav_content>
-                <div style={{display: 'inline-block'}}>
-                {this.state.static_nav_links.map((value, index) => (
-                  <css.header.nav_link key={index} href="/">
-                    <i className={nav_bar_icons[index]}></i> {value}
-                  </css.header.nav_link>
-                ))}
+
+          {/* Align nav bar with yelp logo */}
+          <css.header.nav_content_header>
+
+            {/* Container for all nav links */}
+            <css.header.nav_contents>
+
+              <css.header.nav_link_div>
+                <div style={{ display: 'inline-block' }}>
+                  {this.state.static_nav_links.map((value, index) => (
+                    <css.header.nav_link key={index} href="/">
+                      <i className={nav_bar_icons[index]}></i> {value}
+                    </css.header.nav_link>
+                  ))}
                 </div>
 
-                <div id="beefore"> &#183; </div> 
+                <div id="beefore"> &#183; </div>
 
-                <div style={{display: 'inline-block'}}>
-                {this.state.static_nav_links_2.map((value, index) => (
-                  <css.header.nav_link2 key={index} href="/">
-                  {value}
-                  </css.header.nav_link2>
-                ))}
+                <div style={{ display: 'inline-block' }}>
+                  {this.state.static_nav_links_2.map((value, index) => (
+                    <css.header.nav_link key={index} href="/">
+                      {value}
+                    </css.header.nav_link>
+                  ))}
                 </div>
+              </css.header.nav_link_div>
 
+              <css.header.nav_login_div>
+                <css.header.nav_login href="/">
+                  Login
+                </css.header.nav_login>
+              </css.header.nav_login_div>
 
-              </css.header.nav_content>
-            </css.header.nav_content_header>
-          </css.header_padding>
+            </css.header.nav_contents>
+
+          </css.header.nav_content_header>
+
         </css.header.nav>
+
+        {/* Container for restaurant header */}
+        <css.rest_details.h>
+
+          <css.rest_details.left_h>
+
+            {/* Container to hold title and claim status */}
+            <css.rest_details.title_claim_div>
+              <h1 style={{ fontSize: '38px', fontWeight: 'bold', display: 'inline', paddingRight: '10px' }}>
+                Urbane Cafe
+              </h1>
+
+              <div style={{ display: 'inline-block' }}>
+                <span style={{ width: '18px', height: '18px', paddingRight: '10px'}}>
+                  <i className="far fa-check-circle"></i>
+                </span>
+                Claimed
+              </div>
+            </css.rest_details.title_claim_div>
+            
+            {/* Container to hold stars and reviews count */}
+            <css.rest_details.ratings_div>
+              {/* <div style={{width: '215px', height: '24px'}}> */}
+                <css.rest_details.rating_stars style={{backgroundPosition: '0 -192px'}}>
+                </css.rest_details.rating_stars>
+                <span style={{color: '#666'}}> 46 Reviews </span>
+              {/* </div> */}
+            </css.rest_details.ratings_div>
+
+            <css.rest_details.category_div>
+              <span style={{letterSpacing: '1px'}}> $$   &#183; </span>
+              {this.state.categories.map((value, index) => (
+                <css.rest_details.category href = "/"> {value}, </css.rest_details.category>
+              ))}
+            </css.rest_details.category_div>
+
+          </css.rest_details.left_h>
+
+        </css.rest_details.h>
+
 
       </div> // main div components
     )
--- client/components/style.jsx
+++ client/components/style.jsx
@@ -6,14 +6,30 @@ const headerDiv = styled.div`
 `;
 
 const header_padding = headerDiv.extend`
-  // margin-left: -18px;
-  // margin-right: -18px;
-  // border-collapse: separate;
-  // border-spacing: 18px 0;
+  margin-left: -18px;
+  margin-right: -18px;
+  border-collapse: separate;
+  border-spacing: 18px 0px;
 `;
 
+//create spacing between logo and seach bar
+const header_spacing = header_padding.extend`
+  margin-left: 9px;
+  margin-right: 9px;
+  vertical-algin: middle;
+  display: table-cell;
+`;
+
+const signup_login = styled.a`
+  border: none;
+  text-decoration: none;
+  &:hover{
+    background: rgba(51,51,51,0.3);
+  }
+`;
 
 const header = {
+  // yelp header background
   h: styled.div`
     padding: 12px 0;
     background: #D3231C url(https://s3-media2.fl.yelpcdn.com/assets/srv0/yelp_styleguide/15ee41b0c3b2/assets/img/structural/header_stars@2x.png) no-repeat center;
@@ -21,61 +37,97 @@ const header = {
   `,
 
   //wrapper for logo and search boxes
-  h_content: header_padding.extend`
-    width: 960px;
+  h_content: styled.div`
+    vertical-align: baseline;
+    // width: 960px;
     margin: 0 auto;
     padding: 0 15px;
 
     display: table;
     table-layout: auto;
   `,
-  logo: headerDiv.extend`
+  logo: styled.div`
     background-image: url(https://s3-media1.fl.yelpcdn.com/assets/srv0/yelp_design_web/48792dd29811/assets/img/logos_desktop/default.png);
     background-size: 80px 40px;
     width: 80px;
     height: 40px; 
     display: table-cell;
   `,
-  
+
   search: styled.input`
     display: table-cell;
     width: 285px;
-    padding: 0 30px;
-    border-radius: 4px 0 0 4px;
-    vertical-align: middle;
     height: 36px;
+    padding: 0 30px;
     line-height: 20px;
+    bordeR: none;
+    border-radius: 4px;
+    vertical-align: middle;
   `,
 
   search_button: styled.button`
     display: table-cell;  
     width: 54px;
     height: 36px;
-    border: none:
-    border-radius: 4px 0 0 4px;
+    border: none;
+    border-radius: 4px;
     vertical-align: middle;
     background: #bd1f1f;
     line-height: 36px;
     font-size:100%;
+    color: #fff;
   `,
+
+  divForSignup: styled.div`
+    display: inline-block;
+    vertical-align: middle;
+    height: 36px;
+    width: 104px;
+    margin: 0 15px 0;
+    border: none;
+    border-radius: 4px;
+    
+    text-align: center;
+    background: #bd1f1f;
+  `,
+  signup_button: signup_login.extend`
+    color: #fff;
+    font-weight: bold;
+    line-height: 35px;
+  `,
+
+  // STYLING FOR NAV BAR
+
   //wrapper for whole nav bar
   nav: styled.div` 
     background: #bd1f1f;
     min-width: 1080px;
   `,
-  
+
   //Need this in order to allign with yelp logo
   nav_content_header: styled.div`
-    width: 960px;
+    display: table;
+    width: 920px;
     margin: 0 auto;
     padding: 0 15px;
   `,
 
   //nav bar to hold all a href links
-  nav_content: styled.nav`
-    margin-left: -12px;
-    whitespace: nowrap !important;
+  nav_contents: styled.nav`
+    margin-left: -12px; 
+    // whitespace: nowrap !important;
   `,
+
+  nav_link_div: styled.div`
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    border: 0;
+    font-size: 100%;
+    vertical-align: baseline;
+    display: table-cell;
+  `,
+
   nav_link: styled.a`
     color: #fff;
     display: inline;
@@ -90,22 +142,80 @@ const header = {
       background: rgba(51,51,51,0.3);
       }
   `,
-  nav_link2: styled.a`
+
+  nav_login_div: styled.div`
+    // box-sizing: border-box;
+    display: table-cell;
+    // vertical-align: top;
+    text-align: center;
+  `,
+
+  nav_login: signup_login.extend`
     color: #fff;
-    display: inline;
-    padding: 3px 15px;
-    // font-family: monospace;
+    // display: inline;
+    padding: 0 12px 0;
     font-size: 15px;
     font-weight: bold;
     line-height: 21px;
-    border-radius: 3px;
-    text-decoration: none;
-    &:hover{
-      background: rgba(51,51,51,0.3);
-      }
   `,
 }
 
+const default_div = styled.div`
+  margin: 0;
+  padding: 0;
+  bordeR: 0;
+  font-size: 100%;
+  vertical-align: baseline;
+  display: block;
+`;
+const rest_details = {
+  h: default_div.extend`
+    height: 443px;
+    width: 960px;
+    padding: 10px;
+    margin: 0 296px 0;
+    z-index: 1;
+  `,
+  left_h: default_div.extend`
+    width: 525px;
+    height: 112px;
+    padding-right: 30px;
+    display: inline;
+  `,
+  title_claim_div: default_div.extend`
+    margin-top: 6px !important;
+    width: 525px;
+    height: 42px;
+  `,
+  ratings_div: default_div.extend`
+    margin-top: 15px !important;
+    width: 525px;
+    height: 24px;
+  `,
+
+  rating_stars: default_div.extend`
+    background: url(https://s3-media2.fl.yelpcdn.com/assets/srv0/yelp_design_web/9b34e39ccbeb/assets/img/stars/stars.png);
+    background-size: 132px 560px;
+    display: inline-block;
+    vertical-align-middle;
+    width: 132px;
+    height: 24px;
+    float: left;
+    margin: -4px 6px 0 0;
+  `,
+
+  category_div: default_div.extend`
+    display: inline-block;
+    vertical-align-middle;
+    margin-top: 10px !important;
+    width: 360px;
+    height: 22px;
+  `,
+  category: styled.a`
+    color: #0073bb;
+    text-decoration: none;
+  `,
+}
 module.exports = {
-  header, header_padding
+  header, rest_details, header_spacing
 };
